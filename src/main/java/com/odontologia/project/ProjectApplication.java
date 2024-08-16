e22ad45335d2216210a2df45fb911c6a8dd162f6
David Valdiviezo <aintriago7792@utm.edu.ec>
2024-08-16 11:12:22 -0500

    Added data.sql file with "Odontologos" and "Pacientes" values. Removed "Odontologos" and "Pacientes" addeds manually to database on main function

--- src/main/java/com/odontologia/project/ProjectApplication.java
+++ src/main/java/com/odontologia/project/ProjectApplication.java
@@ -1,43 +1,13 @@
 package com.odontologia.project;
 
-import com.odontologia.project.models.Odontologo;
-import com.odontologia.project.models.Paciente;
-import com.odontologia.project.services.impl.OdontologoService;
-import com.odontologia.project.services.impl.PacienteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class ProjectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectApplication.class, args);
-
-    PacienteService pacienteService = new PacienteService();
-    OdontologoService odontologoService = new OdontologoService();
-
-    Paciente p1 = new Paciente(1313077792L,"David", "Valdiviezo", "Portoviejo", LocalDate.of(2024, 8, 20));
-
-    pacienteService.guardarPaciente(p1);
-//    pacienteService.buscarTodosPacientes();
-//    System.out.println(pacienteService.buscarPaciente(1313077792L));
-//    Paciente p2 = new Paciente(1313077792L,"Andres", "Valdiviezo", "Portoviejo", LocalDate.of(2024, 8, 20));
-//    pacienteService.actualizarPaciente(p2);
-//    pacienteService.eliminarPaciente(1313077792L);
-//    System.out.println(pacienteService.buscarPaciente(1313077792L));
-
-    Odontologo od1 = new Odontologo(1313077795L, "Andres", "Intriago");
-
-    odontologoService.guardarOdontologo(od1);
-//    odontologoService.buscarTodosOdontologos();
-//    System.out.println(odontologoService.buscarOdontologo(1313077795L));
-//    Odontologo od2 = new Odontologo(1313077795L, "David", "Intriago");
-//    odontologoService.actualizarOdontologo(od2);
-//    odontologoService.eliminarOdontologo(1313077795L);
-//    System.out.println(odontologoService.buscarOdontologo(1313077795L));
-
   }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO PACIENTES (DNI, NOMBRE, APELLIDO, DOMICILIO, FECHA_ALTA)
+VALUES (1313077791, 'Jenny', 'Munera', 'Bogota', '2024-05-10'),
+(1313077792, 'Andres', 'Intrago', 'Portoviejo', '2024-08-10')
+(1313077793, 'Carlos', 'Perez', 'Lima', '2025-06-19'),
+(1313077794, 'Alejandro', 'Sosa', 'Mexico', '2024-07-10');
+
+INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO)
+VALUES(1313077791,  'Jenny', 'Munera'),
+(1313077792, 'Carlos', 'Perez'),
+(1313077793, 'Alejandro', 'Sosa'),
+(1313077794, 'Brenda', 'Zarco'),
+(1313077795, 'David', 'Valdiviezo');
