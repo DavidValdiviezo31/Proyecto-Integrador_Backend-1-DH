8eb85fab064ed12a21de9c5e1006bf50dab92250
David Valdiviezo <aintriago7792@utm.edu.ec>
2024-08-16 10:35:23 -0500

    Added PacientesController logic and buscarPaciente.html file for MVC.

--- src/main/java/com/odontologia/project/controllers/PacientesController.java
+++ src/main/java/com/odontologia/project/controllers/PacientesController.java
@@ -1,4 +1,30 @@
 package com.odontologia.project.controllers;
 
+import com.odontologia.project.models.Paciente;
+import com.odontologia.project.services.IPacienteService;
+import com.odontologia.project.services.impl.PacienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("pacientes")
 public class PacientesController {
+  private final IPacienteService pacienteService;
+
+  public PacientesController() {
+    this.pacienteService = new PacienteService();
+  }
+
+  @GetMapping("/buscarPorId")
+  public String buscarPaciente(Model model, @RequestParam Long dni) {
+    Paciente paciente = pacienteService.buscarPaciente(dni);
+
+    model.addAttribute("nombre", paciente.getNombre());
+    model.addAttribute("apellido", paciente.getApellido());
+
+    return "/pacientes/buscarPaciente";
+  }
 }
