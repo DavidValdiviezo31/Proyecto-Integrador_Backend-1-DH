7962465938b522f1d1c92abb47c74fc24eb67241
David Valdiviezo <aintriago7792@utm.edu.ec>
2024-08-21 21:14:02 -0500

    Refactoring methods on controller for adapt them the new implementation "TurnosLOCAL".

--- src/main/java/com/odontologia/project/controllers/TurnosController.java
+++ src/main/java/com/odontologia/project/controllers/TurnosController.java
@@ -10,16 +10,13 @@ import com.odontologia.project.services.impl.OdontologoService;
 import com.odontologia.project.services.impl.PacienteService;
 import com.odontologia.project.services.impl.TurnoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
+import java.util.Objects;
 
-
-@Controller
-@RequestMapping("turnos")
+@RestController
+@RequestMapping("/turnos")
 public class TurnosController {
   private final ITurnoService turnoService;
   private final IPacienteService pacienteService;
@@ -32,50 +29,22 @@ public class TurnosController {
     this.odontologoService = new OdontologoService();
   }
 
-
-  @GetMapping("/buscarPorId")
-  public String buscarPorId(Model model, @RequestParam Long id) {
-    Turno turno = turnoService.buscarTurno(id);
-    if (turno == null) {
-      model.addAttribute("error", "Turno no encontrado");
-      return "error";
-    }
-    Paciente paciente = turno.getPaciente();
-    Odontologo odontologo = turno.getOdontologo();
-
-    model.addAttribute("paciente", paciente.getNombre() + " " + paciente.getApellido());
-    model.addAttribute("odontologo", odontologo.getNombre() + " " + odontologo.getApellido());
-    model.addAttribute("fecha_hora", turno.getFecha().toString() + " " + turno.getHora().toString());
-    model.addAttribute("turno", turno.getId());
-
-    return "/turnos/buscarTurno";
-  }
-
   @PostMapping("/")
-  public ResponseEntity<?> crearTurno(@RequestBody Turno turno) {
-    if (pacienteService.buscarPaciente(turno.getPaciente().getId()) == null) {
-      return ResponseEntity.badRequest().body("Paciente no encontrado");
-    }
-    if (odontologoService.buscarOdontologo(turno.getOdontologo().getId()) == null) {
-      return ResponseEntity.badRequest().body("Odontólogo no encontrado");
-    }
+  public ResponseEntity<Turno> crearTurno(@RequestBody Turno turno, @RequestParam("paciente") Long pacienteId, @RequestParam("odontologo") Long odontologoId) {
+    Paciente paciente = pacienteService.buscarPaciente(pacienteId);
+    Odontologo odontologo = odontologoService.buscarOdontologo(odontologoId);
+
+    if (Objects.isNull(odontologo) || Objects.isNull(paciente)) return ResponseEntity.notFound().build();
+
+    turno.setPaciente(paciente);
+    turno.setOdontologo(odontologo);
+
     return ResponseEntity.ok(turnoService.guardarTurno(turno));
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<?> buscarTurno(@PathVariable Long id) {
-    Turno turno = turnoService.buscarTurno(id);
-    if (turno != null) {
-      if (turno.getPaciente() != null) {
-        turno.getPaciente().setId(turno.getPaciente().getId());
-      }
-      if (turno.getOdontologo() != null) {
-        turno.getOdontologo().setId(turno.getOdontologo().getId());
-      }
-      return ResponseEntity.ok(turno);
-    } else {
-      return ResponseEntity.notFound().build();
-    }
+  public ResponseEntity<Turno> buscarTurno(@PathVariable Long id) {
+    return ResponseEntity.ok(turnoService.buscarTurno(id));
   }
 
   @GetMapping("/")
@@ -84,30 +53,13 @@ public class TurnosController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<?> actualizarTurno(@PathVariable Long id, @RequestBody Turno turno) {
-    if (turnoService.buscarTurno(id) == null) {
-      return ResponseEntity.notFound().build();
-    }
-    if (pacienteService.buscarPaciente(turno.getPaciente().getId()) == null) {
-      return ResponseEntity.badRequest().body("Paciente no encontrado");
-    }
-    if (odontologoService.buscarOdontologo(turno.getOdontologo().getId()) == null) {
-      return ResponseEntity.badRequest().body("Odontólogo no encontrado");
-    }
+  public ResponseEntity<Turno> actualizarTurno(@PathVariable Long id, @RequestBody Turno turno) {
     turno.setId(id);
-    Turno turnoActualizado = turnoService.actualizarTurno(turno);
-    return ResponseEntity.ok(turnoActualizado);
+    return ResponseEntity.ok(turnoService.actualizarTurno(turno));
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<?> eliminarTurno(@PathVariable Long id) {
-    Turno turnoEliminado = turnoService.eliminarTurno(id);
-    if (turnoEliminado != null) {
-      return ResponseEntity.ok(turnoEliminado);
-    } else {
-      return ResponseEntity.notFound().build();
-    }
+  public ResponseEntity<Turno> eliminarTurno(@PathVariable Long id) {
+    return ResponseEntity.ok(turnoService.eliminarTurno(id));
   }
-
-
 }
