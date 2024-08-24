6c8927c6000760b8ea205f8b0949d75e00669cc3
David Valdiviezo <aintriago7792@utm.edu.ec>
2024-08-23 22:22:49 -0500

    Base files for "Pacientes" view.

---
+++ src/main/resources/static/js/pacientes.js
@@ -0,0 +1,12 @@
+const pacienteModel = {
+  dni: 123123123,
+  nombre: 'Juan',
+  apellido: 'Perez',
+  domicilio: {
+    calle: 'Avenida Siempreviva',
+    numero: 742,
+    localidad: 'Springfield',
+    provincia: 'Springfield'
+  },
+  fechaAlta: '2020-01-01'
+}
