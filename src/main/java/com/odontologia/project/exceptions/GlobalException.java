b052f658d02b1ce34fca9cda6bf1e0f8049c05c9
David Valdiviezo <aintriago7792@utm.edu.ec>
2024-09-10 12:56:51 -0500

    Added Logger on Globals Exceptions.

--- src/main/java/com/odontologia/project/exceptions/GlobalException.java
+++ src/main/java/com/odontologia/project/exceptions/GlobalException.java
@@ -1,5 +1,7 @@
 package com.odontologia.project.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,28 +10,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalException {
+  private final Logger logger = LoggerFactory.getLogger(GlobalException.class);
+
   @ExceptionHandler(BadRequestException.class)
   @ResponseBody
   public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+    logger.error(ex.getMessage());
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(InvalidInputException.class)
   @ResponseBody
   public ResponseEntity<String> handleInvalidInputException(InvalidInputException e) {
+    logger.error(e.getMessage());
     return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(EntityNotFoundException.class)
   @ResponseBody
   public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+    logger.error(e.getMessage());
     return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseBody
   public ResponseEntity<String> handleGeneralException(Exception e) {
-    return new ResponseEntity<>("Error interno del servidor: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    logger.error("Error Interno del Servidor: {}", e.getMessage());
+    return new ResponseEntity<>("Error Interno del Servidor: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
 
