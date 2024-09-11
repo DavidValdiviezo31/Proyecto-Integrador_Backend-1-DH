6eb3b70d059a1bde111a9a831cdaecf1a257ebe8
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-09-11 12:19:09 -0500

    refactoring and add method to complete coverage

--- src/test/java/TurnoServiceTest.java
+++ src/test/java/TurnoServiceTest.java
@@ -1,5 +1,8 @@
 import com.odontologia.project.ProjectApplication;
 
+import com.odontologia.project.exceptions.BadRequestException;
+import com.odontologia.project.exceptions.EntityNotFoundException;
+import com.odontologia.project.exceptions.InvalidInputException;
 import com.odontologia.project.models.Domicilio;
 import com.odontologia.project.models.Odontologo;
 import com.odontologia.project.models.Paciente;
@@ -15,6 +18,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -44,23 +48,77 @@ public class TurnoServiceTest {
     Turno turno = new Turno(null,LocalDate.parse("2024-06-24"), LocalTime.parse("10:00"),null,null);
 
     // Act
-    Turno turnoGuardado = turnoService.guardarTurno(turno);
+    Throwable thrown = catchThrowable(() -> turnoService.guardarTurno(turno));
 
     // Assert
-    assertThat(turnoGuardado).isNotNull();
+    assertThat(thrown).isInstanceOf(BadRequestException.class);
+  }
+  @Test
+  public void testGuardarTurnoConFechaNula() {
+    // Arrange
+    Paciente paciente = new Paciente(null, 123456789L, "Pedro", "González", null, LocalDate.parse("2024-01-01"), null);
+    Odontologo odontologo = new Odontologo(null, 123456789L, "Pedro", "González", null);
+    Turno turno = new Turno(null, null, LocalTime.parse("10:00"), paciente, odontologo);
+
+    // Act
+    Throwable thrown = catchThrowable(() -> turnoService.guardarTurno(turno));
+
+    // Assert
+    assertThat(thrown).isInstanceOf(InvalidInputException.class);
+  }
+  @Test
+  public void testGuardarTurnoConHoraNula() {
+    // Arrange
+    Paciente paciente = new Paciente(null, 123456789L, "Pedro", "González", null, LocalDate.parse("2024-01-01"), null);
+    Odontologo odontologo = new Odontologo(null, 123456888L, "Pablo", "Velez", null);
+    Turno turno = new Turno(null, LocalDate.parse("2024-06-24"), null, paciente, odontologo);
+
+    // Act
+    Throwable thrown = catchThrowable(() -> turnoService.guardarTurno(turno));
+
+    // Assert
+    assertThat(thrown).isInstanceOf(InvalidInputException.class);
   }
 
   @Test
   public void testBuscarTurnoPorId() {
     // Arrange
-    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), null, null);
+    Odontologo odontologo = new Odontologo(null, 123456789L, "Pedro", "González", null);
+    Paciente paciente = new Paciente(null, 987654321L, "Luis", "Pérez", null, LocalDate.parse("2024-01-01"), null);
+
+    odontologoService.guardarOdontologo(odontologo);
+    pacienteService.guardarPaciente(paciente);
+
+    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), paciente, odontologo);
     Turno turnoGuardado = turnoService.guardarTurno(turno);
 
     // Act
-    Turno turnoEncontrado = turnoService.buscarTurnoPorId(turnoGuardado.getId());
+    Throwable thrown = catchThrowable(() -> turnoService.buscarTurnoPorId(turnoGuardado.getId()+1));
+    // Assert
+    assertThat(thrown).isInstanceOf(EntityNotFoundException.class);
+  }
+  @Test
+  public void testGuardarTurnoConPacienteNull() {
+    // Arrange
+    Odontologo odontologo = new Odontologo(null, 123456655L, "Hillary", "Villarreal", null);
+    odontologoService.guardarOdontologo(odontologo);
+
+    // Creas un turno con odontólogo pero sin paciente
+    Turno turno = new Turno(null, LocalDate.parse("2024-06-24"), LocalTime.parse("10:00"), null, odontologo);
+
+    // Act
+    Throwable thrown = catchThrowable(() -> turnoService.guardarTurno(turno));
 
     // Assert
-    assertThat(turnoEncontrado).isNotNull();
+    assertThat(thrown).isInstanceOf(BadRequestException.class);
+  }
+  @Test
+  public void testBuscarTurnoPorIdConIdNulo() {
+    // Act
+    Throwable thrown = catchThrowable(() -> turnoService.buscarTurnoPorId(null));
+
+    // Assert
+    assertThat(thrown).isInstanceOf(InvalidInputException.class);
   }
 
   @Test
@@ -72,7 +130,13 @@ public class TurnoServiceTest {
   @Test
   public void testActualizarFechaTurno() {
     // Arrange
-    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), null, null);
+    Odontologo odontologo = new Odontologo(null, 111222444L, "Jose", "Gomez", null);
+    Paciente paciente = new Paciente(null, 987655777L, "Alex", "Peralta", null, LocalDate.parse("2024-07-01"), null);
+
+    odontologoService.guardarOdontologo(odontologo);
+    pacienteService.guardarPaciente(paciente);
+
+    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), paciente, odontologo);
     Turno turnoGuardado = turnoService.guardarTurno(turno);
 
     // Act
@@ -86,7 +150,13 @@ public class TurnoServiceTest {
   @Test
   public void testActualizarHoraTurno() {
     // Arrange
-    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), null, null);
+    Odontologo odontologo = new Odontologo(null, 111222333L, "Pablo", "Gomez", null);
+    Paciente paciente = new Paciente(null, 987655667L, "Luisa", "Peralta", null, LocalDate.parse("2024-05-01"), null);
+
+    odontologoService.guardarOdontologo(odontologo);
+    pacienteService.guardarPaciente(paciente);
+
+    Turno turno = new Turno(null, LocalDate.parse("2023-04-29"), LocalTime.parse("08:00"), paciente, odontologo);
     Turno turnoGuardado = turnoService.guardarTurno(turno);
 
     // Act
@@ -100,15 +170,23 @@ public class TurnoServiceTest {
   @Test
   public void testEliminarTurnoPorId() {
     // Arrange
-    Turno turno = new Turno(null, LocalDate.parse("2024-06-24"), LocalTime.parse("13:00"), null, null);
+    Odontologo odontologo = new Odontologo(null, 111222333L, "Pablo", "Gomez", null);
+    Paciente paciente = new Paciente(null, 987655667L, "Luisa", "Peralta", null, LocalDate.parse("2024-05-01"), null);
+
+    odontologoService.guardarOdontologo(odontologo);
+    pacienteService.guardarPaciente(paciente);
+
+    Turno turno = new Turno(null, LocalDate.parse("2024-06-24"), LocalTime.parse("13:00"), paciente, odontologo);
     Turno turnoGuardado = turnoService.guardarTurno(turno);
 
     // Act
     turnoService.eliminarTurnoPorId(turnoGuardado.getId());
-    Turno turnoEncontrado = turnoService.buscarTurnoPorId(turnoGuardado.getId());
+    //Turno turnoEncontrado = turnoService.buscarTurnoPorId(turnoGuardado.getId());
+    Throwable thrown = catchThrowable(() -> turnoService.buscarTurnoPorId(turnoGuardado.getId()));
 
     // Assert
-    assertThat(turnoEncontrado).isNull();
+    //assertThat(turnoEncontrado).isNull();
+    assertThat(thrown).isInstanceOf(EntityNotFoundException.class);
   }
 
   @Test
